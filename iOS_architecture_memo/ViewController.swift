cd4187d36cbe67c796566ee649b1120c69b71e6f
Takeshi-Komori <stubgurssy@gmail.com>
2019-08-15 11:52:09 +0900

    make project

---
+++ iOS_architecture_memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_architecture_memo
+//
+//  Created by 小森武史 on 2019/08/15.
+//  Copyright © 2019 com.komori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
